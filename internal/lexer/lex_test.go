e90d7b0870124153ebc7c10e883704012fd0b22c
correalm <luismigueldasilva.99@gmail.com>
2025-01-15 14:13:11 -0300

    move lex to lexer package

--- internal/lexer/lex_test.go
+++ internal/lexer/lex_test.go
@@ -1,7 +1,7 @@
-package parser_test
+package lexer_test
 
 import (
-	"json_parser/internal/parser"
+	"json_parser/internal/lexer"
 	"reflect"
 	"testing"
 	"strings"
@@ -9,7 +9,7 @@ import (
 
 func TestLexer(t *testing.T) {
   t.Run("returns the correct tokens", func(t *testing.T) {
-    result := parser.Lex("\"{\"foo\":\"bar\",\"bar\":\"foo\", \"number\": 4}\"")
+    result := lexer.Lex("\"{\"foo\":\"bar\",\"bar\":\"foo\", \"number\": 4}\"")
     expected := []string{"{", "foo", ":", "bar", ",", "bar", ":", "foo", "number", "4", "}"}
 
     if !reflect.DeepEqual(result, expected) {
@@ -20,7 +20,7 @@ func TestLexer(t *testing.T) {
 
 func TestLexString(t *testing.T) {
 	t.Run("returns the correct result", func(t *testing.T) {
-		result, index, _ := parser.LexString("\"foo_bar\"")
+		result, index, _ := lexer.LexString("\"foo_bar\"")
 
 		expected := "\"foo_bar\""
     expected_idex := 8
@@ -35,7 +35,7 @@ func TestLexString(t *testing.T) {
 	})
 
 	t.Run("returns an error when the passed string is no terminated", func(t *testing.T) {
-		result, index, err := parser.LexString("\"foo_bar")
+		result, index, err := lexer.LexString("\"foo_bar")
 
 		if result != "" {
 			t.Fatalf("Expected empty string, got %s", result)
@@ -60,7 +60,7 @@ func TestLexString(t *testing.T) {
 
 func TestLexInteger(t *testing.T) {
   t.Run("returns the correct value", func(t *testing.T) {
-    result, index := parser.LexInteger("250,")
+    result, index := lexer.LexInteger("250,")
 
     if result != 250 {
       t.Fatalf("Expected 250, got %d", result)
@@ -72,7 +72,7 @@ func TestLexInteger(t *testing.T) {
   })
 
   t.Run("returns the correct value for negative values", func(t *testing.T) {
-    result, index := parser.LexInteger("-45 ")
+    result, index := lexer.LexInteger("-45 ")
 
     if result != -45 {
       t.Fatalf("Expected -45, got %d", result)
@@ -86,7 +86,7 @@ func TestLexInteger(t *testing.T) {
 
 func TestLexFloat(t *testing.T) {
   t.Run("returns the correct value", func(t *testing.T) {
-    result, index := parser.LexFloat("22.2 ")
+    result, index := lexer.LexFloat("22.2 ")
     expected := 22.2
 
     if result != expected {
@@ -99,7 +99,7 @@ func TestLexFloat(t *testing.T) {
   })
 
   t.Run("returns the correct value for negative values", func(t *testing.T) {
-    result, index := parser.LexFloat("-789.08,")
+    result, index := lexer.LexFloat("-789.08,")
     expected := -789.08
 
     if result != expected {
